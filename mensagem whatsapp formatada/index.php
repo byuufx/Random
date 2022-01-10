a0cb19286e7c7d0b5c89f9147c0664a3a70b96c7
felipe <36082241+byuufx@users.noreply.github.com>
2022-01-09 21:43:29 -0300

    Create index.php

---
+++ mensagem whatsapp formatada/index.php
@@ -0,0 +1,46 @@
+<?php
+
+
+/**
+ * Whatsapp
+ *
+ * @param  string $numero  número no padrão DDD do país + DDD região + número
+ * @param  string $titulo  título
+ * @param  array $texto  array para montar a mensagem 
+ * @return string O link com a mensagem formatada.
+ */
+function Whatsapp(string $numero = '', string $titulo = '', array $texto = []){
+
+  if(empty($numero) || empty($titulo) || empty($texto)){
+    return false;
+  }
+
+  $nl = "%0A"; //newline
+
+  $text = "-- *$titulo* -- $nl $nl";
+  
+  foreach($texto as $key => $value){
+    $text .= "*$key :* $value $nl";
+  }
+  
+  $link = "https://wa.me/$numero?text=$text";
+  return $link;
+}
+
+
+$titulo = "Cardápio Lanchonete Legal";
+
+$arr = array(
+  "X-Tudo" => "R$ 19,00",
+  "X-Queijo" => "R$ 12,00",
+  "X-Batata" => "R$ 16,00",
+  "X-Ratão" => "R$ 12,00",
+  "Batata frita 500g" => "R$ 30,00"
+);
+
+$numero = "5511934345722"; // número de exemplo - bot do Ifood
+
+echo Whatsapp($numero, $titulo, $arr);
+
+
+
